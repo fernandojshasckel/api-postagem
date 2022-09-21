343c8778cf224784664a1e644ed713f88464d05d
fernandojshasckel <fernandojosehasckel@gmail.com>
2022-09-21 15:52:50 -0700

    001

---
+++ src/main/java/br/edu/unisep/tads/apipostagem/model/Comentario.java
@@ -0,0 +1,16 @@
+package br.edu.unisep.tads.apipostagem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "comentario")
+public class Comentario {
+    
+
+    
+}
